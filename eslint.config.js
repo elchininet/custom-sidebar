d1445ebc0904d43ff99908a2d7f2dee7bff38b7c
Iván Pereira Muñoz <elchininet@gmail.com>
2024-04-08 01:31:51 +0200

    Migrate eslint config and update Playwright docker image

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const tseslint = require('typescript-eslint');
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+	{
+		languageOptions: {
+			globals: {
+				Atomics: 'readonly',
+				SharedArrayBuffer: 'readonly',
+				...globals.browser,
+				...globals.node,
+				...globals.es2018
+			}
+		}
+	},
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		rules: {
+			quotes: [
+				'error',
+				'single',
+				{
+					avoidEscape: true,
+					allowTemplateLiterals: true
+				}
+			],
+			indent: ['error', 4],
+			semi: ['error', 'always'],
+			'comma-dangle': ['error', 'never'],
+			'no-trailing-spaces': ['error'],
+			'array-bracket-spacing': ['error', 'never'],
+			'@typescript-eslint/no-duplicate-enum-values': 'off',
+			'comma-spacing': ['error'],
+			'@typescript-eslint/no-var-requires': 'off'
+		}
+	}
+];
