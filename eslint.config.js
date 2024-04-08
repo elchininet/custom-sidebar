0e8a4ef243c26c122c09ac95c85b2b41c54f4508
Iván Pereira Muñoz <elchininet@gmail.com>
2024-04-09 00:20:18 +0200

    Some fixes to the Eslint fla configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,37 +3,37 @@ const js = require('@eslint/js');
 const globals = require('globals');
 
 module.exports = [
-	{
-		languageOptions: {
-			globals: {
-				Atomics: 'readonly',
-				SharedArrayBuffer: 'readonly',
-				...globals.browser,
-				...globals.node,
-				...globals.es2018
-			}
-		}
-	},
-	js.configs.recommended,
-	...tseslint.configs.recommended,
-	{
-		rules: {
-			quotes: [
-				'error',
-				'single',
-				{
-					avoidEscape: true,
-					allowTemplateLiterals: true
-				}
-			],
-			indent: ['error', 4],
-			semi: ['error', 'always'],
-			'comma-dangle': ['error', 'never'],
-			'no-trailing-spaces': ['error'],
-			'array-bracket-spacing': ['error', 'never'],
-			'@typescript-eslint/no-duplicate-enum-values': 'off',
-			'comma-spacing': ['error'],
-			'@typescript-eslint/no-var-requires': 'off'
-		}
-	}
+    {
+        languageOptions: {
+            globals: {
+                Atomics: 'readonly',
+                SharedArrayBuffer: 'readonly',
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2020
+            }
+        }
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            quotes: [
+                'error',
+                'single',
+                {
+                    avoidEscape: true,
+                    allowTemplateLiterals: true
+                }
+            ],
+            indent: ['error', 4],
+            semi: ['error', 'always'],
+            'comma-dangle': ['error', 'never'],
+            'no-trailing-spaces': ['error'],
+            'array-bracket-spacing': ['error', 'never'],
+            '@typescript-eslint/no-duplicate-enum-values': 'off',
+            'comma-spacing': ['error'],
+            '@typescript-eslint/no-var-requires': 'off'
+        }
+    }
 ];
