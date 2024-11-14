329fb40973d9b7836d32d2fddd842ebc5b2c7a94
Iván Pereira Muñoz <elchininet@gmail.com>
2024-11-14 21:30:59 +0100

    Fix extending from base

---
+++ tests/04 - item-color-options-extending.spec.ts
@@ -0,0 +1,90 @@
+import { test, expect } from 'playwright-test-coverage';
+import { Page } from '@playwright/test';
+import { CONFIG_FILES, SIDEBAR_CLIP } from './constants';
+import { haConfigRequest, fulfillJson } from './utilities';
+import { SELECTORS } from './selectors';
+
+const getSelectedOrderItemExtendingColorOption = (
+    optionBase: {
+        option: string;
+        value: string;
+    },
+    option: string
+) => ({
+    [optionBase.option]: optionBase.value,
+    order: [
+        {
+            item: 'overview',
+            [option]: 'red'
+        }
+    ]
+});
+
+test.beforeAll(async () => {
+    await haConfigRequest(CONFIG_FILES.BASIC);
+});
+
+const pageVisit = async (page: Page): Promise<void> => {
+    await page.goto('/');
+    await expect(page.locator(SELECTORS.HA_SIDEBAR)).toBeVisible();
+    await expect(page.locator(SELECTORS.HUI_VIEW)).toBeVisible();
+};
+
+[
+    {
+        title: 'should set global icon_color_selected getting the value of icon_color',
+        json: getSelectedOrderItemExtendingColorOption(
+            {
+                option: 'icon_color_selected',
+                value: 'var(--custom-sidebar-icon-color)'
+            },
+            'icon_color'
+        ),
+        screenshot: '01-icon-color-selected-from-icon-color.png'
+    },
+    {
+        title: 'should set global text_color_selected getting the value of text_color',
+        json: getSelectedOrderItemExtendingColorOption(
+            {
+                option: 'text_color_selected',
+                value: 'var(--custom-sidebar-text-color)'
+            },
+            'text_color'
+        ),
+        screenshot: '02-text-color-selected-from-text-color.png'
+    },
+    {
+        title: 'should set global selection_color getting the value of text_color',
+        json: getSelectedOrderItemExtendingColorOption(
+            {
+                option: 'selection_color',
+                value: 'var(--custom-sidebar-text-color)'
+            },
+            'text_color'
+        ),
+        screenshot: '03-selection-color-from-text-color.png'
+    },
+    {
+        title: '@testing should set global selection_opacity',
+        json: getSelectedOrderItemExtendingColorOption(
+            {
+                option: 'selection_opacity',
+                value: '1'
+            },
+            'selection_color'
+        ),
+        screenshot: '04-global-selection-opacity.png'
+    }
+].forEach(({ title, json, screenshot }) => {
+
+    test(title, async ({ page }) => {
+
+        await fulfillJson(page, json);
+        await pageVisit(page);
+        await expect(page).toHaveScreenshot(screenshot, {
+            clip: SIDEBAR_CLIP
+        });
+
+    });
+
+});
