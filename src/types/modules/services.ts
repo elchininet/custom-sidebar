f6b6df5205853546ea0644ec2bfcffb160afa553
Iván Pereira Muñoz <elchininet@gmail.com>
2025-05-10 16:30:37 +0200

    Refactor callService method and add some warnings

--- src/types/modules/services.ts
+++ src/types/modules/services.ts
@@ -7,11 +7,3 @@ export interface CheckConfigResponse {
     errors: string | null;
     warnings: string | null;
 }
-
-export interface EntityState {
-    entity_id: string;
-    last_changed: string;
-    last_reported: string;
-    last_updated: string;
-    state: string;
-}
