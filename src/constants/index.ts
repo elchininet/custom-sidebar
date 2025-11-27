2e591583d0e48593a3e544b870165877082026f0
Iván Pereira Muñoz <elchininet@gmail.com>
2025-11-27 02:09:51 +0100

    Simplify the logic to select the sidebar item that has been selected taking into account the URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -275,5 +275,3 @@ export const JINJA_TEMPLATE_REG = /\{\{[\s\S]*\}\}|\{%[\s\S]*%\}/;
 export const PARTIAL_REGEXP = /@partial\s+([\w-]+)/g;
 export const DOMAIN_ENTITY_REGEXP = /^\s*([a-z_]+)\.([\w-]+)\s*$/;
 export const REF_VARIABLE_REGEXP = /^\s*ref\(([\s\S]*)\)\s*$/;
-
-export const URL_WITH_PARAMS_REGEXP = /^([^?]+).*$/;
