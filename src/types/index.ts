adbab80ad76802472909dc4180904b4125f9e0f6
Iván Pereira Muñoz <elchininet@gmail.com>
2026-02-01 03:03:09 +0100

    Update get-promisable-result package and remove local PromisableOptions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,7 @@
-import { getPromisableResult } from 'get-promisable-result';
-
 export * from './modules/config';
 export * from './modules/elements';
 export * from './modules/primitives';
 export * from './modules/services';
-
-// TODO: export PromisableOptions from get-promisable-result
-export type PromisableOptions = Parameters<typeof getPromisableResult>[2];
 
 declare global {
     interface Window {
