fed35593576c4be99ed9a57c13470ff46d1ca2f3
Iván Pereira Muñoz <elchininet@gmail.com>
2026-02-08 17:38:18 +0100

    Create navigate action and JavaScript methods

--- src/utilities/modules/navigate.ts
+++ src/utilities/modules/navigate.ts
@@ -1,4 +1,11 @@
-import { EVENT } from '@constants';
+import { HAElement } from 'home-assistant-query-selector';
+import { SidebarItem } from '@types';
+import {
+    CLASS,
+    CUSTOM_ELEMENT,
+    EVENT,
+    NAMESPACE
+} from '@constants';
 
 const dispatchLocationChanged = (pathname: string): void => {
     window.dispatchEvent(
@@ -13,12 +20,44 @@ const dispatchLocationChanged = (pathname: string): void => {
     );
 };
 
-export const navigate = (pathname: string): void => {
-    const params: Parameters<typeof window.history.replaceState> = [
-        null,
-        '',
-        pathname
-    ];
-    window.history.replaceState(...params);
-    dispatchLocationChanged(pathname);
+export const navigate = (
+    pathname: string,
+    replace = false,
+    warningMessage = 'ignoring navigate method using the path'
+): void => {
+    if (pathname.startsWith('/')) {
+        const params: Parameters<typeof window.history.replaceState> = [
+            null,
+            '',
+            pathname
+        ];
+        if (replace) {
+            window.history.replaceState(...params);
+        } else {
+            window.history.pushState(...params);
+        }
+        dispatchLocationChanged(pathname);
+    } else {
+        console.warn(`${NAMESPACE}: ${warningMessage} "${pathname}" as it doesn't start with "/".`);
+    }
+};
+
+export const buildNavigateMethods = (sidebar: HAElement) => {
+
+    return {
+        navigate,
+        activateItem: async (item: SidebarItem) => {
+
+            // Small delay to avoid activating the item before the panel load logic runs
+            await new Promise((resolve) => setTimeout(resolve, 5));
+            const activeItem = await sidebar.selector.$.query(`${CUSTOM_ELEMENT.ITEM}.${CLASS.ITEM_SELECTED}`).element as HTMLElement;
+
+            activeItem.classList.remove(CLASS.ITEM_SELECTED);
+            activeItem.tabIndex = -1;
+
+            item.classList.add(CLASS.ITEM_SELECTED);
+            item.tabIndex = 0;
+
+        }
+    };
 };
