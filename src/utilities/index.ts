a38c6fbdbb1e0c5b54a4a74e9230b8ad90ac80c9
Iván Pereira Muñoz <elchininet@gmail.com>
2026-05-01 20:28:05 +0200

    Prepare the plugin for the next Home Assistant version

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,7 +1,7 @@
 export { getConfig } from './modules/config';
 export { getTemplateWithPartials } from './modules/partials';
 export { getCSSVariables } from './modules/css';
-export { randomId, parseWidth } from './modules/misc';
+export { randomId, parseWidth, isMobileClient } from './modules/misc';
 export { getRestApis } from './modules/restApi';
 export * from './modules/navigate';
 export * from './modules/dialogs';
